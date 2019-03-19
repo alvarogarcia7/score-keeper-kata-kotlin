d0aac33206d7b1286fb5e91917f44308636c3279
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-03-19 01:33:43 +0100

    Green

--- src/main/kotlin/ScoreKeeper.kt
+++ src/main/kotlin/ScoreKeeper.kt
@@ -1,13 +1,18 @@
 package com.example.kata.scorekeeper
 
 class ScoreKeeper {
-    fun scoreTeamA1() {}
+    private lateinit var score: String
+
+    fun scoreTeamA1() {
+        score = "001:000"
+    }
+
     fun scoreTeamA2() {}
     fun scoreTeamA3() {}
     fun scoreTeamB1() {}
     fun scoreTeamB2() {}
     fun scoreTeamB3() {}
     fun getScore(): String {
-        return ""
+        return score
     }
 }
