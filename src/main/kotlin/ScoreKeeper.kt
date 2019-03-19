58d1c710c0939406212259a19fe4451604855a16
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-03-19 01:45:52 +0100

    Refactor: parallel change - use a new algorithm for splitting the scores into two variables

--- src/main/kotlin/ScoreKeeper.kt
+++ src/main/kotlin/ScoreKeeper.kt
@@ -5,36 +5,55 @@ class ScoreKeeper {
 
     private var scaffold: Boolean = false
 
+    private var scoreA: Int = 0
+    private var scoreB: Int = 0
+
     fun scoreTeamA1() {
         if (scaffold) {
+            scoreA = 2
             score = "002:000"
         } else {
+            scoreA = 1
             score = "001:000"
             scaffold = true
         }
     }
 
     fun scoreTeamA2() {
+        scoreA = 2
         score = "002:000"
     }
 
     fun scoreTeamA3() {
+        scoreA = 3
         score = "003:000"
     }
 
     fun scoreTeamB1() {
+        scoreB = 1
         score = "000:001"
     }
 
     fun scoreTeamB2() {
+        scoreB = 2
         score = "000:002"
     }
 
     fun scoreTeamB3() {
+        scoreB = 3
         score = "000:003"
     }
 
     fun getScore(): String {
+        if (formatScore().equals(score)) {
+            return formatScore()
+        } else {
+            println("This case does not match: ${formatScore()} vs $score")
+        }
         return score
+    }
+
+    private fun formatScore(): String {
+        return String.format("%03d:%03d", scoreA, scoreB)
     }
 }
