534188fa3b42e1591f9b60fd8c57553d73cefe17
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-03-19 01:47:25 +0100

    Refactor: finish parallel change - remove old way of setting the score

--- src/main/kotlin/ScoreKeeper.kt
+++ src/main/kotlin/ScoreKeeper.kt
@@ -1,7 +1,6 @@
 package com.example.kata.scorekeeper
 
 class ScoreKeeper {
-    private lateinit var score: String
 
     private var scaffold: Boolean = false
 
@@ -11,46 +10,34 @@ class ScoreKeeper {
     fun scoreTeamA1() {
         if (scaffold) {
             scoreA = 2
-            score = "002:000"
         } else {
             scoreA = 1
-            score = "001:000"
             scaffold = true
         }
     }
 
     fun scoreTeamA2() {
         scoreA = 2
-        score = "002:000"
     }
 
     fun scoreTeamA3() {
         scoreA = 3
-        score = "003:000"
     }
 
     fun scoreTeamB1() {
         scoreB = 1
-        score = "000:001"
     }
 
     fun scoreTeamB2() {
         scoreB = 2
-        score = "000:002"
     }
 
     fun scoreTeamB3() {
         scoreB = 3
-        score = "000:003"
     }
 
     fun getScore(): String {
-        if (formatScore() == score) {
-            return formatScore()
-        } else {
-            println("This case does not match: ${formatScore()} vs $score")
-        }
-        return score
+        return formatScore()
     }
 
     private fun formatScore(): String {
