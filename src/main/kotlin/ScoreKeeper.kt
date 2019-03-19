a151cbfcbeaa31f0868a482fcfa7dd1e72f83719
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-03-19 01:40:29 +0100

    Green

--- src/main/kotlin/ScoreKeeper.kt
+++ src/main/kotlin/ScoreKeeper.kt
@@ -5,6 +5,7 @@ class ScoreKeeper {
 
     fun scoreTeamA1() {
         score = "001:000"
+
     }
 
     fun scoreTeamA2() {
@@ -15,9 +16,18 @@ class ScoreKeeper {
         score = "003:000"
     }
 
-    fun scoreTeamB1() {}
-    fun scoreTeamB2() {}
-    fun scoreTeamB3() {}
+    fun scoreTeamB1() {
+        score = "000:001"
+    }
+
+    fun scoreTeamB2() {
+        score = "000:002"
+    }
+
+    fun scoreTeamB3() {
+        score = "000:003"
+    }
+
     fun getScore(): String {
         return score
     }
